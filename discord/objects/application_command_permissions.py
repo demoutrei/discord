9611fe6e37798b0092aee45beb39a54f22421945
demoutrei <works.demoutrei@gmail.com>
2026-07-22 16:34:19 +0800

    Added 'Application Command Permissions' object

--- discord/objects/application_command_permissions.py
+++ discord/objects/application_command_permissions.py
@@ -1,5 +1,19 @@
+from ..enums import ApplicationCommandPermissionType
+from ..snowflake import Snowflake
 from ._base import BaseObject
 
 
 class ApplicationCommandPermissions(BaseObject):
-  ...
+  """Application command permissions allow you to enable or disable commands for specific users, roles, or channels within a guild."""
+
+  id: Snowflake
+  """ID of the role, user, or channel. It can also be a `permission constant`_
+
+  _ permission constant: :class:`~discord.enums.ApplicationCommandPermissionsConstants`
+  """
+
+  permission: bool
+  """``True`` to allow, ``False`` to disallow"""
+
+  type: ApplicationCommandPermissionType
+  """Role, user, or channel"""
