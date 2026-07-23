a711b0cd6699ca5fe4a39bdd8764be2ea500bf91
demoutrei <works.demoutrei@gmail.com>
2026-07-24 00:14:09 +0800

    Added 'Auto Moderation Action' object

---
+++ discord/objects/auto_moderation_action.py
@@ -0,0 +1,14 @@
+# from ..enums import AutoModerationActionType
+from ..utils import Optional
+from ._base import BaseObject
+# from .auto_moderation_action_metadata import AutoModerationActionMetadata
+
+
+class AutoModerationAction(BaseObject):
+  """An action which will execute whenever a rule is triggered."""
+
+  metadata: Optional["AutoModerationActionMetadata"]
+  """Additional metadata needed during execution for this specific action type"""
+
+  type: "AutoModerationActionType"
+  """The type of action"""
