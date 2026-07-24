3b26cd13d19ceaae2f681ea62d712ecafd2fa1bb
demoutrei <works.demoutrei@gmail.com>
2026-07-24 20:31:13 +0800

    Build documentation

--- discord/objects/channel.py
+++ discord/objects/channel.py
@@ -40,7 +40,7 @@ class Channel(BaseObject):
   default_reaction_emoji: Optional[Nullable["DefaultReaction"]]
   """The emoji to show in the add reaction button on a thread in a :attr:`~discord.enums.ChannelType.GUILD_FORUM` or a :attr:`~discord.enums.ChannelType.GUILD_MEDIA` channel"""
 
-  default_sort_order: Optional[Nullable["DefaultSortOrderType"]]
+  default_sort_order: Optional[Nullable[DefaultSortOrderType]]
   """The default sort order type used to order posts in :attr:`~discord.enums.ChannelType.GUILD_FORUM` and :attr:`~discord.enums.ChannelType.GUILD_MEDIA` channels. Defaults to ``None``, which indicates a preferred sort order hasn't been set by a channel admin"""
 
   default_thread_rate_limit_per_user: Optional[int]
