af1a9c57c040868609d01bdeb317f092f2f975f9
demoutrei <works.demoutrei@gmail.com>
2026-07-27 20:26:45 +0800

    Added 'ThreadMembersUpdateEvent' event object

---
+++ discord/events/thread_members_update.py
@@ -0,0 +1,25 @@
+from ..objects import ThreadMember
+from ..snowflake import Snowflake
+from ..utils import Optional
+from ._base import DispatchEvent
+
+
+class ThreadMembersUpdateEvent(DispatchEvent):
+  added_members: Optional[list[ThreadMember]]
+  """Users who were added to the thread.
+
+  .. note::
+      In this gateway event, the thread member objects will also include the guild member and nullable presence objects for each added thread member.
+  """
+  
+  guild_id: Snowflake
+  """ID of the guild."""
+  
+  id: Snowflake
+  """ID of the thread."""
+
+  member_count: int
+  """Approximate number of members in the thread, capped at 50."""
+
+  removed_member_ids: Optional[list[Snowflake]]
+  """ID of the users who were removed from the thread."""
