2e87dd09eef8d2b32f94ee6b648c7d17323d599a
demoutrei <works.demoutrei@gmail.com>
2026-07-27 19:08:18 +0800

    Added 'ChannelInfoEvent' event object

--- discord/events/__init__.py
+++ discord/events/__init__.py
@@ -1,4 +1,5 @@
 from ._base import DispatchEvent
 from ._manager import EventManager
 from .auto_moderation_action_execution import AutoModerationActionExecutionEvent
+from .channel_info import ChannelInfoEvent
 from .ready import ReadyEvent
